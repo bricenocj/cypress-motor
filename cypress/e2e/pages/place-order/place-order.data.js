172e4593648e70ef0d8f8de84d3538b8ba5fe6f6
exb87922 <carlos.briceno@claro.com.ar>
2023-07-31 13:20:51 -0300

    Creación pagina Place-Order

---
+++ cypress/e2e/pages/place-order/place-order.data.js
@@ -0,0 +1,14 @@
+export class PlaceOrderData {
+    static get testDate() {
+        return {
+            name: 'Test Name',
+            country: 'Argentina',
+            city: 'Córdoba',
+            credicardNumber: '4012888888881881',
+            month: 12,
+            year: 2024,
+
+        }
+    }
+
+}
