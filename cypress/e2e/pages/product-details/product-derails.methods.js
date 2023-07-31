e7078b9100ef710a5585f2d0c941e5f8cff4e9ea
exb87922 <carlos.briceno@claro.com.ar>
2023-07-31 08:03:25 -0300

    Cración de archivos correspondientes a Products Detail Pages

---
+++ cypress/e2e/pages/product-details/product-derails.methods.js
@@ -0,0 +1,8 @@
+import { ProductDetailElements } from "./product-details.elements";
+
+export class ProductsDetailsMethods{
+    static clickOnAddToCartButton(){
+        ProductDetailElements.buttons.addToCart.click();
+    }
+
+}
