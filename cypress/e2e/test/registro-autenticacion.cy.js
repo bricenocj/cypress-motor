02e18bb2250aac19bf69b5c2064a27a05b933ec2
exb87922 <carlos.briceno@claro.com.ar>
2023-08-07 10:18:31 -0300

    Crear usuario Aleatorio

--- cypress/e2e/test/registro-autenticacion.cy.js
+++ cypress/e2e/test/registro-autenticacion.cy.js
@@ -2,7 +2,8 @@ import { CommonPageData } from "../pages/common-page/common-page.data";
 import { CommonPageMethods } from "../pages/common-page/common-page.methods";
 import { SignupMethods } from "../pages/signup/signup.methods";
 import { Logger } from "../util/logger";
-
+const user = CommonPageMethods.generateRandomString();
+const password = CommonPageMethods.generateRandomString(7);
 describe(CommonPageData.testSuite.registroYAunteticacion, ()=>{
     it('<Registro de usuario Válido', ()=>{
         Logger.stepNumber(1)
@@ -16,8 +17,8 @@ describe(CommonPageData.testSuite.registroYAunteticacion, ()=>{
         Logger.stepNumber(3)
         Logger.step('Completar todos los campos obligatorios con información válida')
         //SignupMethods.signup(bricenocj, cucho123)
-        SignupMethods.insertUsername('bricenocj')
-        SignupMethods.insertPassword('cucho123')
+        SignupMethods.insertUsername(user)
+        SignupMethods.insertPassword(password)
 
         Logger.stepNumber(4)
         Logger.step('Hacer clik en "Sign up" para registrar el usuario')
